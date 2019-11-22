92e512d8f5be028f3664b3d4c5475f37ae626392
ThejeshwarAB <42470612+ThejeshwarAB@users.noreply.github.com>
2019-11-22 14:12:34 +0530

    Create ap.c

---
+++ in-c/ap.c
@@ -0,0 +1,26 @@
+//sourcecode
+
+#include<stdio.h>
+#include<stdlib.h>
+main()
+{
+int a,b,c;
+scanf("%d %d %d",&a,&b,&c);
+while(c)
+{
+    printf("%d ",a);
+    a+=b;
+    c--;
+}
+}
+//a is first element
+//b is common diff
+//c is count no
+
+/*
+Sample IP:
+1 2 4 
+
+Sample OP:
+1 3 5 7
+*/
